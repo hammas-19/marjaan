193848603d3d586b7e897276d18c83048039084a
hammas-19 <hammasmasood@gmail.com>
2024-11-04 19:43:40 +0500

    chore: MInor content Changes

--- components/display2.vue
+++ components/display2.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="grid grid-cols-2 md:gap-5 gap-1 md:h-[calc(100vh-120px)] h-[calc(100vh-80px)]">
 
-        <GridContainer :title="'Error 404ss'" :slogan="'lets make it worth'" :has-products="hasProducts" image="/gridImages/collection1.webp" class="h-full min-w-full" />
-        <GridContainer :title="'Error 404ss'" :slogan="'lets make it worth'" :has-products="hasProducts" image="/gridImages/collection2.webp" class="h-full min-w-full md:row-span-2" />
-        <GridContainer :title="'Error 404sas'" :slogan="'lets make it worth'" :has-products="hasProducts" image="/gridImages/collection3.webp" class="h-full min-w-full col-span-2 md:col-span-1" />
+        <GridContainer :title="'Core'" :slogan="'lets make it worth'" :has-products="hasProducts" image="/gridImages/core.png" class="h-full min-w-full" />
+        <GridContainer :title="'AURA'" :slogan="'lets make it worth'" :has-products="hasProducts" image="/gridImages/tokyo.jpg" class="h-full min-w-full md:row-span-2" />
+        <GridContainer :title="'NyctoPhille'" :slogan="'lets make it worth'" :has-products="hasProducts" image="/gridImages/flora.png" class="h-full min-w-full col-span-2 md:col-span-1" />
 
     </div>
 </template>
