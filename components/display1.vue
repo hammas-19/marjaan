b1237eaf79bdba34e5e5b169bcc2720141cf78c4
hammas-19 <hammasmasood@gmail.com>
2024-11-08 18:54:42 +0500

    chore: minor changes

--- components/display1.vue
+++ components/display1.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="grid grid-cols-2 md:gap-5 gap-1 md:h-[calc(100vh-120px)] h-[calc(100vh-80px)]">
 
-        <GridContainer :title="'Error 404ss'" :slogan="'lets make it worth'" :has-products="hasProducts" image="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg4zIhCWHjxYgbYydkswPFBYsjMTeAZ7T1AtJd1psL-cWbHaP3Mq_J8aW6e3KzqDz-Sx1Zp63QeqR-c1DXPw7oDn0IEINieuELN8itasMFEf2p2UPaQW70UUS_Y6sVrL6X0uS9SraKD6hISB7KVNj7elhnSVs_LnrFJb9t3ZtjdnEPnAZyW6oWXbh93yaY/s320/404.gif" class="h-full min-w-full row-span-2">
+        <GridContainer :title="'AURA'" :slogan="'Radiate Confidence'" :has-products="hasProducts" image="/gridImages/flora.png" class="h-full min-w-full row-span-2">
             <template #item1>
                <NuxtLink to="/productsTees">
                     T-Shirts
@@ -19,7 +19,7 @@
             </template>
         </GridContainer>
 
-        <GridContainer :title="'Funky Fresco'" :slogan="'lets make it worth'" :has-products="hasProducts" :content-left="false" image="/gridImages/collection3.webp" class="min-w-full h-full">
+        <GridContainer :title="'NyctoPhille'" :slogan="'Where Darkness Inspires'" :has-products="hasProducts" :content-left="false" image="/gridImages/henna.jpg" class="min-w-full h-full">
             <template #item1>
                <NuxtLink to="/productsTees">
                     T-Shirts
@@ -36,7 +36,7 @@
                 </NuxtLink>
             </template>
         </GridContainer>
-        <GridContainer :title="'Restored'" :slogan="'lets make it worth'" :has-products="hasProducts" :content-left="false" image="/gridImages/collection4.webp" class="min-w-full h-full">
+        <GridContainer :title="'Restored'" :slogan="'Elegance, Renewed'" :has-products="hasProducts" :content-left="false" image="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg4zIhCWHjxYgbYydkswPFBYsjMTeAZ7T1AtJd1psL-cWbHaP3Mq_J8aW6e3KzqDz-Sx1Zp63QeqR-c1DXPw7oDn0IEINieuELN8itasMFEf2p2UPaQW70UUS_Y6sVrL6X0uS9SraKD6hISB7KVNj7elhnSVs_LnrFJb9t3ZtjdnEPnAZyW6oWXbh93yaY/s320/404.gif" class="min-w-full h-full">
             <template #item1>
                <NuxtLink to="/productsTees">
                     T-Shirts
